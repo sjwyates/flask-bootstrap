1faf5ce010f133b88617d41c624ead5c69eca685
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-11-22 15:54:47 -0700

    app setup

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, render_template
+from sassutils.wsgi import SassMiddleware
 
 
 app = Flask(__name__)
 
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/scss', 'static/css', '/static/css')
+})
 
 @app.route("/")
 def home():
