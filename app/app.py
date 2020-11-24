1244bb503a27e7874c7f5c246621d03e84d83156
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-11-24 16:51:48 -0700

    added pages with j2 extensions

--- app/app.py
+++ app/app.py
@@ -10,4 +10,8 @@ app.wsgi_app = SassMiddleware(app.wsgi_app, {
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('pages/landing.html.j2', message="hello world")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
