c38d5e9af1282039ead66a1c68bbe0e8bab03dda
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-11-24 17:25:15 -0700

    nav links

--- app/app.py
+++ app/app.py
@@ -9,8 +9,23 @@ app.wsgi_app = SassMiddleware(app.wsgi_app, {
 })
 
 @app.route("/")
-def home():
-    return render_template('pages/landing.html.j2', message="hello world")
+def landing():
+    return render_template('pages/landing.html.j2')
+
+
+@app.route("/visualizations")
+def visualizations():
+    return render_template('pages/visualizations.html.j2')
+
+
+@app.route("/comparisons")
+def comparisons():
+    return render_template('pages/comparisons.html.j2')
+
+
+@app.route("/data")
+def data():
+    return render_template('pages/data.html.j2')
 
 
 if __name__ == "__main__":
